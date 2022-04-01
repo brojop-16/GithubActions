5d93c230d5d64d82ecacf4e48f852a9f77ac3e31
JohnDelfosse <69905691+JohnDelfosse@users.noreply.github.com>
2022-04-01 17:53:25 -0500

    made "new change" to Power
    Basically forgot to save the first time

--- Tests/UnitTests.cs
+++ Tests/UnitTests.cs
@@ -31,5 +31,110 @@ namespace GithubActionsLab
         }
 
         // Implement 3 tests per operation, following a similar pattern as above
+
+        //Subtraction Tests
+
+        [Test]
+        public void Sub_ValidDelfosse()
+        {
+            //first one is wrong, should be zero
+            Assert.AreEqual(0, Program.Subtract("1", "2"));
+            Assert.AreEqual(1, Program.Subtract("3", "2"));
+            Assert.AreEqual(3, Program.Subtract("5", "2"));
+        }
+
+        [Test]
+        public void Sub_InvalidDelfosse()
+        {
+            Assert.Throws<FormatException>(() => Program.Subtract("1", "a"));
+            Assert.Throws<FormatException>(() => Program.Subtract("a", "1"));
+            Assert.Throws<FormatException>(() => Program.Subtract("a", "a"));
+        }
+
+        [Test]
+        public void Sub_NullDelfosse()
+        {
+            Assert.Throws<ArgumentNullException>(() => Program.Subtract("1", null));
+            Assert.Throws<ArgumentNullException>(() => Program.Subtract(null, "1"));
+            Assert.Throws<ArgumentNullException>(() => Program.Subtract(null, null));
+        }
+
+        //Multiplication Tests
+
+        [Test]
+        public void Mult_ValidDelfosse()
+        {
+            Assert.AreEqual(2, Program.Multiply("1", "2"));
+            Assert.AreEqual(6, Program.Multiply("3", "2"));
+            Assert.AreEqual(10, Program.Multiply("5", "2"));
+        }
+
+        [Test]
+        public void Mult_InvalidDelfosse()
+        {
+            Assert.Throws<FormatException>(() => Program.Multiply("1", "a"));
+            Assert.Throws<FormatException>(() => Program.Multiply("a", "1"));
+            Assert.Throws<FormatException>(() => Program.Multiply("a", "a"));
+        }
+
+        [Test]
+        public void Mult_NullDelfosse()
+        {
+            Assert.Throws<ArgumentNullException>(() => Program.Multiply("1", null));
+            Assert.Throws<ArgumentNullException>(() => Program.Multiply(null, "1"));
+            Assert.Throws<ArgumentNullException>(() => Program.Multiply(null, null));
+        }
+
+        //Division tests
+
+        [Test]
+        public void Div_ValidDelfosse()
+        {
+            Assert.AreEqual(2, Program.Divide("2", "1"));
+            Assert.AreEqual(2, Program.Divide("4", "2"));
+            Assert.AreEqual(1, Program.Divide("5", "5"));
+        }
+
+        [Test]
+        public void Div_InvalidDelfosse()
+        {
+            Assert.Throws<FormatException>(() => Program.Divide("1", "a"));
+            Assert.Throws<FormatException>(() => Program.Divide("a", "1"));
+            Assert.Throws<FormatException>(() => Program.Divide("a", "a"));
+        }
+
+        [Test]
+        public void Div_NullDelfosse()
+        {
+            Assert.Throws<ArgumentNullException>(() => Program.Divide("1", null));
+            Assert.Throws<ArgumentNullException>(() => Program.Divide(null, "1"));
+            Assert.Throws<ArgumentNullException>(() => Program.Divide(null, null));
+        }
+
+        //Power Tests
+
+        [Test]
+        public void Pow_ValidDelfosse()
+        {
+            Assert.AreEqual(2, Program.Power("2", "1"));
+            Assert.AreEqual(16, Program.Power("4", "2"));
+            Assert.AreEqual(3125, Program.Power("5", "5"));
+        }
+
+        [Test]
+        public void Pow_InvalidDelfosse()
+        {
+            Assert.Throws<FormatException>(() => Program.Power("1", "a"));
+            Assert.Throws<FormatException>(() => Program.Power("a", "1"));
+            Assert.Throws<FormatException>(() => Program.Power("a", "a"));
+        }
+
+        [Test]
+        public void Pow_NullDelfosse()
+        {
+            Assert.Throws<ArgumentNullException>(() => Program.Power("1", null));
+            Assert.Throws<ArgumentNullException>(() => Program.Power(null, "1"));
+            Assert.Throws<ArgumentNullException>(() => Program.Power(null, null));
+        }
     }
 }
