801bafe7817f4ab9a28c0d440b28f7bb73760658
Khoa Le <69731752+KHOA011@users.noreply.github.com>
2022-04-02 22:26:49 -0500

    Update UnitTests.cs
    Edit test suit

--- Tests/UnitTests.cs
+++ Tests/UnitTests.cs
@@ -6,24 +6,24 @@ namespace GithubActionsLab
     [TestFixture]
     public class Math
     {
-        [Test]
-        public void Add_Valid()
+        [TestLe]
+        public void Add_Valid_Le()
         {
             Assert.AreEqual(3, Program.Add("1", "2"));
             Assert.AreEqual(5, Program.Add("3", "2"));
             Assert.AreEqual(12, Program.Add("5", "7"));
         }
 
-        [Test]
-        public void Add_Invalid()
+        [TestLe]
+        public void Add_Invalid_Le()
         {
             Assert.Throws<FormatException>(() => Program.Add("1", "a"));
             Assert.Throws<FormatException>(() => Program.Add("a", "1"));
             Assert.Throws<FormatException>(() => Program.Add("a", "a"));
         }
 
-        [Test]
-        public void Add_Null()
+        [TestLe]
+        public void Add_Null_Le()
         {
             Assert.Throws<ArgumentNullException>(() => Program.Add("1", null));
             Assert.Throws<ArgumentNullException>(() => Program.Add(null, "1"));
@@ -31,5 +31,109 @@ namespace GithubActionsLab
         }
 
         // Implement 3 tests per operation, following a similar pattern as above
+        
+        //SUBTRACTION
+        [TestLe]
+        public void Sub_Valid_Le()
+        {
+            Assert.AreEqual(1, Program.Subtract("3", "2"));
+            Assert.AreEqual(3, Program.Subtract("6", "3"));
+            //WRONG TEST
+            Assert.AreEqual(1, Program.Subtract("5", "7"));
+        }
+
+        [TestLe]
+        public void Sub_Invalid_Le()
+        {
+            Assert.Throws<FormatException>(() => Program.Subtract("1", "a"));
+            Assert.Throws<FormatException>(() => Program.Subtract("a", "1"));
+            Assert.Throws<FormatException>(() => Program.Subtract("a", "a"));
+        }
+
+        [TestLe]
+        public void Sub_Null_Le()
+        {
+            Assert.Throws<ArgumentNullException>(() => Program.Subtract("1", null));
+            Assert.Throws<ArgumentNullException>(() => Program.Subtract(null, "1"));
+            Assert.Throws<ArgumentNullException>(() => Program.Subtract(null, null));
+        }
+        
+        
+        //MULTIPLICATION
+        [TestLe]
+        public void Mul_Valid_Le()
+        {
+            Assert.AreEqual(6, Program.Multiply("3", "2"));
+            Assert.AreEqual(18, Program.Multiply("6", "3"));
+            Assert.AreEqual(35, Program.Multiply("5", "7"));
+        }
+
+        [TestLe]
+        public void Sub_Invalid_Le()
+        {
+            Assert.Throws<FormatException>(() => Program.Multiply("1", "a"));
+            Assert.Throws<FormatException>(() => Program.Multiply("a", "1"));
+            Assert.Throws<FormatException>(() => Program.Multiply("a", "a"));
+        }
+
+        [TestLe]
+        public void Sub_Null_Le()
+        {
+            Assert.Throws<ArgumentNullException>(() => Program.Subtract("1", null));
+            Assert.Throws<ArgumentNullException>(() => Program.Subtract(null, "1"));
+            Assert.Throws<ArgumentNullException>(() => Program.Subtract(null, null));
+        }
+        
+        
+        //  DIVISION
+        [TestLe]
+        public void Di_Valid_Le()
+        {
+            Assert.AreEqual(3, Program.Divide("6", "2"));
+            Assert.AreEqual(6, Program.Divide("18", "3"));
+            Assert.AreEqual(5, Program.Divide("35", "7"));
+        }
+
+        [TestLe]
+        public void Di_Invalid_Le()
+        {
+            Assert.Throws<FormatException>(() => Program.Divide("1", "a"));
+            Assert.Throws<FormatException>(() => Program.Divide("a", "1"));
+            Assert.Throws<FormatException>(() => Program.Divide("a", "a"));
+        }
+
+        [TestLe]
+        public void Di_Null_Le()
+        {
+            Assert.Throws<ArgumentNullException>(() => Program.Divide("1", null));
+            Assert.Throws<ArgumentNullException>(() => Program.Divide(null, "1"));
+            Assert.Throws<ArgumentNullException>(() => Program.Divide(null, null));
+        }
+        
+        //POWER
+        [TestLe]
+        public void Power_Valid_Le()
+        {
+            Assert.AreEqual(4, Program.Power("2", "2"));
+            Assert.AreEqual(9, Program.Power("3", "2"));
+            Assert.AreEqual(16, Program.Power("4", "2"));
+        }
+
+        [TestLe]
+        public void Power_Invalid_Le()
+        {
+            Assert.Throws<FormatException>(() => Program.Power("1", "a"));
+            Assert.Throws<FormatException>(() => Program.Power("a", "1"));
+            Assert.Throws<FormatException>(() => Program.Power("a", "a"));
+        }
+
+        [TestLe]
+        public void Power_Null_Le()
+        {
+            Assert.Throws<ArgumentNullException>(() => Program.Power("1", null));
+            Assert.Throws<ArgumentNullException>(() => Program.Power(null, "1"));
+            Assert.Throws<ArgumentNullException>(() => Program.Power(null, null));
+        }
+        
     }
 }
