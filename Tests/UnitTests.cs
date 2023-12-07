7c3bfaff673d6f137b0b9f0ef7d930b59622fd4b
Leo Gao <zgao11@huskers.unl.edu>
2023-12-06 23:11:11 -0600

    number changes

--- Tests/UnitTests.cs
+++ Tests/UnitTests.cs
@@ -35,7 +35,7 @@ namespace GithubActionsLab
         [Test]
         public void Subtract_Valid()
         {
-            Assert.AreEqual(1, Program.Subtract("3", "2"));
+            Assert.AreEqual(1, Program.Subtract("4", "3"));
             Assert.AreEqual(-1, Program.Subtract("2", "3"));
             Assert.AreEqual(0, Program.Subtract("5", "5"));
         }
