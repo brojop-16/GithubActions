dbe8301d9794f012ac8684f81d13e806b67fb34f
reva-long <140870008+reva-long@users.noreply.github.com>
2024-04-07 14:55:12 -0500

    Update Program.cs

--- Console/Program.cs
+++ Console/Program.cs
@@ -81,10 +81,12 @@ namespace GithubActionsLab
             return double.Parse(x) / double.Parse(y);
         }
 
-        // Implement this method following a similar pattern as above
+       
         public static double Power(string x, string y)
         {
-            throw new NotImplementedException();
+            double baseValue = double.Parse(x);
+            double exponent = double.Parse(y);
+            return Math.Pow(baseValue, exponent);
         }
     }
 
