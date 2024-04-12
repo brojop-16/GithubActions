9060bf21d1b5ac7161453269da79e9260c719a06
Grant Kerrigan <gkerrigan2@unl.edu>
2024-04-12 12:14:00 -0500

    fixed tests cases I forgot to make them correctly

--- Tests/UnitTests.cs
+++ Tests/UnitTests.cs
@@ -57,73 +57,73 @@ namespace GithubActionsLab
         [Test]
         public void Add_Valid_Mult_GrantKerrigan()
         {
-            Assert.AreEqual(2, Program.Subtract("1", "2"));
-            Assert.AreEqual(6, Program.Subtract("3", "2"));
-            Assert.AreEqual(35, Program.Subtract("5", "7"));
+            Assert.AreEqual(2, Program.Multiply("1", "2"));
+            Assert.AreEqual(6, Program.Multiply("3", "2"));
+            Assert.AreEqual(35, Program.Multiply("5", "7"));
         }
 
         [Test]
         public void Add_Invalid_Mult_GrantKerrigan()
         {
-            Assert.Throws<FormatException>(() => Program.Subtract("1", "a"));
-            Assert.Throws<FormatException>(() => Program.Subtract("a", "1"));
-            Assert.Throws<FormatException>(() => Program.Subtract("a", "a"));
+            Assert.Throws<FormatException>(() => Program.Multiply("1", "a"));
+            Assert.Throws<FormatException>(() => Program.Multiply("a", "1"));
+            Assert.Throws<FormatException>(() => Program.Multiply("a", "a"));
         }
 
         [Test]
         public void Add_Null_Mult_GrantKerrigan()
         {
-            Assert.Throws<ArgumentNullException>(() => Program.Subtract("1", null));
-            Assert.Throws<ArgumentNullException>(() => Program.Subtract(null, "1"));
-            Assert.Throws<ArgumentNullException>(() => Program.Subtract(null, null));
+            Assert.Throws<ArgumentNullException>(() => Program.Multiply("1", null));
+            Assert.Throws<ArgumentNullException>(() => Program.Multiply(null, "1"));
+            Assert.Throws<ArgumentNullException>(() => Program.Multiply(null, null));
         }
 
         [Test]
         public void Add_Valid_Div_GrantKerrigan()
         {
-            Assert.AreEqual(1, Program.Subtract("2", "2"));
-            Assert.AreEqual(3, Program.Subtract("6", "2"));
-            Assert.AreEqual(5, Program.Subtract("35", "7"));
+            Assert.AreEqual(1, Program.Divide("2", "2"));
+            Assert.AreEqual(3, Program.Divide("6", "2"));
+            Assert.AreEqual(5, Program.Divide("35", "7"));
         }
 
         [Test]
         public void Add_Invalid_Div_GrantKerrigan()
         {
-            Assert.Throws<FormatException>(() => Program.Subtract("1", "a"));
-            Assert.Throws<FormatException>(() => Program.Subtract("a", "1"));
-            Assert.Throws<FormatException>(() => Program.Subtract("a", "a"));
+            Assert.Throws<FormatException>(() => Program.Divide("1", "a"));
+            Assert.Throws<FormatException>(() => Program.Divide("a", "1"));
+            Assert.Throws<FormatException>(() => Program.Divide("a", "a"));
         }
 
         [Test]
         public void Add_Null_Div_GrantKerrigan()
         {
-            Assert.Throws<ArgumentNullException>(() => Program.Subtract("1", null));
-            Assert.Throws<ArgumentNullException>(() => Program.Subtract(null, "1"));
-            Assert.Throws<ArgumentNullException>(() => Program.Subtract(null, null));
+            Assert.Throws<ArgumentNullException>(() => Program.Divide("1", null));
+            Assert.Throws<ArgumentNullException>(() => Program.Divide(null, "1"));
+            Assert.Throws<ArgumentNullException>(() => Program.Divide(null, null));
         }
 
         [Test]
         public void Add_Valid_Pow_GrantKerrigan()
         {
-            Assert.AreEqual(1, Program.Subtract("1", "1"));
-            Assert.AreEqual(9, Program.Subtract("3", "2"));
-            Assert.AreEqual(25, Program.Subtract("5", "2"));
+            Assert.AreEqual(1, Program.Power("1", "1"));
+            Assert.AreEqual(9, Program.Power("3", "2"));
+            Assert.AreEqual(25, Program.Power("5", "2"));
         }
 
         [Test]
         public void Add_Invalid_Pow_GrantKerrigan()
         {
-            Assert.Throws<FormatException>(() => Program.Subtract("1", "a"));
-            Assert.Throws<FormatException>(() => Program.Subtract("a", "1"));
-            Assert.Throws<FormatException>(() => Program.Subtract("a", "a"));
+            Assert.Throws<FormatException>(() => Program.Power("1", "a"));
+            Assert.Throws<FormatException>(() => Program.Power("a", "1"));
+            Assert.Throws<FormatException>(() => Program.Power("a", "a"));
         }
 
         [Test]
         public void Add_Null_Pow_GrantKerrigan()
         {
-            Assert.Throws<ArgumentNullException>(() => Program.Subtract("1", null));
-            Assert.Throws<ArgumentNullException>(() => Program.Subtract(null, "1"));
-            Assert.Throws<ArgumentNullException>(() => Program.Subtract(null, null));
+            Assert.Throws<ArgumentNullException>(() => Program.Power("1", null));
+            Assert.Throws<ArgumentNullException>(() => Program.Power(null, "1"));
+            Assert.Throws<ArgumentNullException>(() => Program.Power(null, null));
         }
 
     }
