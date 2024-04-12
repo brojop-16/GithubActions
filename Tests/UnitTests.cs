c73ed2b7ca831a924d9fcee900675c1c4b22fff9
Grant Kerrigan <gkerrigan2@unl.edu>
2024-04-12 11:20:37 -0500

    added unit tests for the rest of the operators

--- Tests/UnitTests.cs
+++ Tests/UnitTests.cs
@@ -30,6 +30,101 @@ namespace GithubActionsLab
             Assert.Throws<ArgumentNullException>(() => Program.Add(null, null));
         }
 
-        // Implement 3 tests per operation, following a similar pattern as above
+        [Test]
+        public void Add_Valid_Sub_GrantKerrigan()
+        {
+            Assert.AreEqual(-1, Program.Subtract("1", "2"));
+            Assert.AreEqual(1, Program.Subtract("3", "2"));
+            Assert.AreEqual(-2, Program.Subtract("5", "7"));
+        }
+
+        [Test]
+        public void Add_Invalid_Sub_GrantKerrigan()
+        {
+            Assert.Throws<FormatException>(() => Program.Subtract("1", "a"));
+            Assert.Throws<FormatException>(() => Program.Subtract("a", "1"));
+            Assert.Throws<FormatException>(() => Program.Subtract("a", "a"));
+        }
+
+        [Test]
+        public void Add_Null_Sub_GrantKerrigan()
+        {
+            Assert.Throws<ArgumentNullException>(() => Program.Subtract("1", null));
+            Assert.Throws<ArgumentNullException>(() => Program.Subtract(null, "1"));
+            Assert.Throws<ArgumentNullException>(() => Program.Subtract(null, null));
+        }
+
+        [Test]
+        public void Add_Valid_Mult_GrantKerrigan()
+        {
+            Assert.AreEqual(2, Program.Subtract("1", "2"));
+            Assert.AreEqual(6, Program.Subtract("3", "2"));
+            Assert.AreEqual(35, Program.Subtract("5", "7"));
+        }
+
+        [Test]
+        public void Add_Invalid_Mult_GrantKerrigan()
+        {
+            Assert.Throws<FormatException>(() => Program.Subtract("1", "a"));
+            Assert.Throws<FormatException>(() => Program.Subtract("a", "1"));
+            Assert.Throws<FormatException>(() => Program.Subtract("a", "a"));
+        }
+
+        [Test]
+        public void Add_Null_Mult_GrantKerrigan()
+        {
+            Assert.Throws<ArgumentNullException>(() => Program.Subtract("1", null));
+            Assert.Throws<ArgumentNullException>(() => Program.Subtract(null, "1"));
+            Assert.Throws<ArgumentNullException>(() => Program.Subtract(null, null));
+        }
+
+        [Test]
+        public void Add_Valid_Div_GrantKerrigan()
+        {
+            Assert.AreEqual(1, Program.Subtract("2", "2"));
+            Assert.AreEqual(3, Program.Subtract("6", "2"));
+            Assert.AreEqual(5, Program.Subtract("35", "7"));
+        }
+
+        [Test]
+        public void Add_Invalid_Div_GrantKerrigan()
+        {
+            Assert.Throws<FormatException>(() => Program.Subtract("1", "a"));
+            Assert.Throws<FormatException>(() => Program.Subtract("a", "1"));
+            Assert.Throws<FormatException>(() => Program.Subtract("a", "a"));
+        }
+
+        [Test]
+        public void Add_Null_Div_GrantKerrigan()
+        {
+            Assert.Throws<ArgumentNullException>(() => Program.Subtract("1", null));
+            Assert.Throws<ArgumentNullException>(() => Program.Subtract(null, "1"));
+            Assert.Throws<ArgumentNullException>(() => Program.Subtract(null, null));
+        }
+
+        [Test]
+        public void Add_Valid_Pow_GrantKerrigan()
+        {
+            Assert.AreEqual(4, Program.Subtract("2", "1"));
+            Assert.AreEqual(9, Program.Subtract("3", "2"));
+            Assert.AreEqual(25, Program.Subtract("5", "2"));
+        }
+
+        [Test]
+        public void Add_Invalid_Pow_GrantKerrigan()
+        {
+            Assert.Throws<FormatException>(() => Program.Subtract("1", "a"));
+            Assert.Throws<FormatException>(() => Program.Subtract("a", "1"));
+            Assert.Throws<FormatException>(() => Program.Subtract("a", "a"));
+        }
+
+        [Test]
+        public void Add_Null_Pow_GrantKerrigan()
+        {
+            Assert.Throws<ArgumentNullException>(() => Program.Subtract("1", null));
+            Assert.Throws<ArgumentNullException>(() => Program.Subtract(null, "1"));
+            Assert.Throws<ArgumentNullException>(() => Program.Subtract(null, null));
+        }
+
     }
 }
